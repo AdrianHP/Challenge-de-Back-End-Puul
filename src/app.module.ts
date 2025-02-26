a99ea67974654d37814808060e1d2e4718afc52c
AdrianHP <adrianmatcom@gmail.com>
2025-02-25 18:51:28 -0600

    code structure change

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+// import { UsersModule } from './users/users.module';
+// import { TasksModule } from './tasks/tasks.module';
+// import { AnalyticsModule } from './analytics/analytics.module';
+import { DatabaseModule } from './data/database.module';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
